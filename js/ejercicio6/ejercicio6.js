394a9527570f22b7427d8e161a742d864ce7aff2
Ruben Torres <rtorresgutierrez.guadalupe@alumnado.fundacionloyola.net>
2021-09-29 20:32:01 +0200

    Ejercicio6 resuelto

--- js/ejercicio6/ejercicio6.js
+++ js/ejercicio6/ejercicio6.js
@@ -2,18 +2,54 @@
 
 import {Empleado} from './empleado.js';
 
-class Empleado {
+class Empresa {
+
   constructor() {
+    this.gasto = 0;
     this.empleados = [];
-    this.crearEMpleados(5);
+    this.crearEmpleados(5);
     window.onload = this.iniciar.bind(this);
   }
+
   iniciar(){
     this.leerSueldos();
-    console.log(this.contarEntre(100, 300));
-    console.log(this.contarMasDe(300));
-    console.log(this.getGastoTotal());
+    this.dividirEntre(300);
+    this.getGasto();
   }
+
+  crearEmpleados(num){
+    for (let i = 0; i < num; i++)
+      this.empleados.push(new Empleado());
+  }
+
+  leerSueldos(){ 
+    for(let i=0;i<this.empleados.length;i++){
+      this.empleados[i].sueldo = parseInt(prompt(`Sueldo empleado ${i+1}: (Entre 100 y 500)`));
+      if(isNaN(this.empleados[i].sueldo) || this.empleados[i].sueldo > 500 || this.empleados[i].sueldo < 100){
+        alert('No se ha introducido un valor válido.');
+        i--;
+      }
+    } 
+  }
+
+  dividirEntre(max){
+    let mayor = 0;
+    let menor = 0;
+
+    for (let empleado of this.empleados)
+      if (empleado.sueldo>max)
+        mayor++;
+      else menor++;
+    console.log(`Hay ${mayor} empleados que cobran más de $300`);
+    console.log(`Hay ${menor} empleados que cobran hasta $300`);
+  }
+
+  getGasto(){
+    for (let empleado of this.empleados)
+      this.gasto += empleado.sueldo;
+    console.log(`El gasto total es de $${this.gasto}.`);
+  }
+
 }
 
-new Empleado();
+new Empresa();
--- js/ejercicio6/empleado.js
+++ js/ejercicio6/empleado.js
@@ -1,30 +1,9 @@
 //Rubén Torres
 
 export class Empleado {
+  
   constructor() {
+    this.sueldo;
+  }
 
-  }
-  crearEmpleados(num){
-    for (let i = 0; i < num; i++) {
-      this.empleado.push(new Empleado());
-    }
-  }
-  leerSueldos(){
-    for (let empleado of this.empleado) {
-      empleado.sueldo = Math.floor((Math.random() * 500) + 100);
-      //empleado.sueldo = parseInt(prompt(`Sueldo empleado ${i}:`));
-      if(isNaN(empleado.sueldo)){
-        alert('No se ha introducido un valor válido.');
-        i--;
-      }
-    }
-  }
-  informarEntre(min, max){
-    for (let empleado of this.empleado) {
-      gasto+=empleados[i];
-      if (empleados[i]>300)
-        mayor++;
-      else menor++;
-    }
-  }
 }
